00fc6e286bc3bb86d3127a226587c178acc34e64
alekseyevd <alekseyev.d@gmail.com>
2020-12-13 15:36:57 +0300

    fix: excel dashboard list

--- src/core/modules/excel/components/Dashboard.js
+++ src/core/modules/excel/components/Dashboard.js
@@ -1,12 +1,16 @@
 import tpl from './tpl/Dashboard.html'
+import list from './tpl/List.html'
 import _ from '@core/tools/_'
 import getExcelListFromStorage from '../helpers/functions'
 import {Loader} from '../helpers/Loader'
 
-
 export class ExcelDashboardView extends HTMLElement {
   constructor() {
     super()
+    this.data = {
+      loading: true
+    }
+
     const $template = document.createElement('template')
     if (typeof(tpl) === 'string') {
       $template.innerHTML = tpl
@@ -20,6 +24,22 @@ export class ExcelDashboardView extends HTMLElement {
   async connectedCallback() {
     const data = await getExcelListFromStorage()
     console.log(data);
+    this.render(data)
+  }
+
+  render(data = []) {
+    const $template = document.createElement('template')
+    $template.innerHTML = list
+    const ul = $template.content.querySelector('ul')
+    data.forEach(el => {
+      const clone = $template.content.querySelector('template')
+          .content.cloneNode(true)
+      clone.querySelector('a').textContent = el.title
+      clone.querySelector('strong').textContent = el.date
+      ul.appendChild(clone)
+    })
+    this.querySelector('loader-component').
+        replaceWith($template.content.cloneNode(true))
   }
 }
 
